cab3692e47b21ade4b93fff8259a37218d09b291
Tucker <tcozzens91@gmail.com>
2019-07-10 13:49:15 -0600

    Call to lamba for posting to db

--- signalr-api/Controllers/MessageController.cs
+++ signalr-api/Controllers/MessageController.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
 using SignalRHub;
 using System;
+using System.Net.Http;
+using System.Text;
 
 namespace SignalR_Hub.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[EnableCors("CorsPolicy")]
     public class MessageController : ControllerBase
     {
         private IHubContext<NotifyHub, ITypedHubClient> _hubContext;
@@ -25,7 +27,12 @@ namespace SignalR_Hub.Controllers
 
             try
             {
+                var client = new HttpClient();
+                client.PostAsync("https://5lqbx47uxa.execute-api.us-east-1.amazonaws.com/dev/messages", new StringContent(msg.Payload, Encoding.UTF8, "application/json"));
+
                 _hubContext.Clients.All.BroadcastMessage(msg.Type, msg.Payload);
+
+
                 retMessage = "Success";
             }
             catch (Exception e)
